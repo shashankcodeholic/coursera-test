77427d0a386a0b45f92a20206c296504b48b4925
codeholicshashank <shashank@codeholic.in>
2023-03-15 11:08:18 +0530

    Module 5 Coding Assignment bug fix.

--- module5-solution/js/script2.js
+++ module5-solution/js/script2.js
@@ -29,8 +29,7 @@ $(function () {
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
     var menuItemsUrl =
-    //   "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/SP.json";
-        "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/"
+      "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/SP.json";
     var menuItemsTitleHtml = "snippets/menu-items-title.html";
     var menuItemHtml = "snippets/menu-item.html";
   
